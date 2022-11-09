24ac4e68da1f97541669c3d600716762c0a60c1d
ALFIAN-code <allvvnt@gmail.com>
2022-11-10 00:23:33 +0700

    update

--- lib/randomWordScreen.dart
+++ lib/randomWordScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:english_words/english_words.dart';
-import 'savedPageScreen.dart';
+import 'savedWordScreen.dart';
 
 class randomWord extends StatefulWidget {
   @override
